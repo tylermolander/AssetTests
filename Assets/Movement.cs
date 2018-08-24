abf1d76ef490ff4dc1596683da9b5a312bc0186d
Tyler Molander <tylermolander@gmail.com>
2018-08-24 16:15:17 -0400

    TestInitial

---
+++ Assets/Movement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[AddComponentMenu("Control Script/FPS Input")]
+public class Movement : MonoBehaviour
+{
+
+    public float speed = 0.6f;
+    public float gravity = -9.8f;
+
+    private CharacterController _charController;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _charController = GetComponent<CharacterController>();    
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float deltaX = Input.GetAxis("Horizontal") * speed;	
+	}
+}
---
+++ Assets/Rotation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube : MonoBehaviour {
+
+
+    public float speed = 0.3f;
+
+	
+	void Update ()
+    {
+        transform.Rotate(speed, speed, speed);
+	}
+}
