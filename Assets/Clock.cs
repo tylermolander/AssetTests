473209a6b4d28d0c6e6027d4b2100de1bf620dad
Tyler Molander <tylermolander@gmail.com>
2018-08-24 22:17:45 -0400

    FailedClockButNotReally

--- Assets/Clock.cs
+++ Assets/Clock.cs
@@ -1,16 +1,30 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
 using UnityEngine;
 
-public class Clock : MonoBehaviour {
+public class Clock : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    const float degreesPerHour = 30f;
+    const float degreesPerMinute = 6f;
+    const float degreesPerSecond = 6f;
+
+    public Transform hoursTransform;
+    public Transform minutesTransform;
+    public Transform secondsTransform;
+
+
+
+
+    void Update()
+    {
+        DateTime time = DateTime.Now;
+        secondsTransform.localRotation =
+           Quaternion.Euler(0f, time.Second * degreesPerSecond, 0f);
+        minutesTransform.localRotation =
+            Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
+        hoursTransform.localRotation =
+            Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
+    }
+
+
 }
